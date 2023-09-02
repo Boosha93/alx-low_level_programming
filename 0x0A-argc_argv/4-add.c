c12736a6bafb8bb1e580881407e0a69c9a730005
Boosha93 <138696335+Boosha93@users.noreply.github.com>
2023-09-02 04:27:25 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,40 +1,33 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
 
 /**
-*main - adds positive numbers
-*@argc: number of arguments
-*@argv: array of arguments
-*Return: 0 on success, 1 on failure
-*/
-
-/*
-1. The main function takes in the number of arguments and an array of strings.
-2. The first for loop iterates through the arguments.
-3. The second for loop iterates through each character of the argument.
-4. If the character is not a digit, the program prints “Error” and returns 1.
-5. If the character is a digit, the program adds the number to the sum.
-6. The program prints the sum and returns 0.
-*/
+ * main - add
+ * @argc:int
+ * @argv:string
+ * Return:int
+ */
 int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
+	int x = 0, i, j;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
+			if (isdigit(argv[i][j]) == 0)
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-
-		sum += atoi(argv[i]);
 	}
-
-	printf("%d\n", sum);
-
+	for (i = 1; i < argc; i++)
+	{
+		x += atoi(argv[i]);
+	}
+	printf("%d\n", x);
 	return (0);
 }
